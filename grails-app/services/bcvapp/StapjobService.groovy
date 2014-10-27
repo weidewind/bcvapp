74c880f258c5f034b2cec6dff2e977ef91048f1b
weidewind <weidewind@gmail.com>
2014-10-27 16:20:50 +0300

    previous commit works; now I add modes (stap or pipeline)

--- grails-app/services/bcvapp/StapjobService.groovy
+++ grails-app/services/bcvapp/StapjobService.groovy
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 import grails.util.Mixin
-
+import grails.util.Holders
 import org.codehaus.groovy.grails.web.context.ServletContextHolder as SCH
 
 
