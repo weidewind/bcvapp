7ed681d043a8e5cf4ba8052d28b01e2f6399e06d
weidewind <weidewind@gmail.com>
2014-10-27 14:08:27 +0300

    port 25

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -66,7 +66,7 @@ grails {
 	  username = "bcv.pipeline@gmail.com"
 	  password = "ETRApagadi"
 	  props = ["mail.smtp.auth":"true",
-			   "mail.smtp.socketFactory.port":"465",
+			   "mail.smtp.socketFactory.port":"25",
 			   "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
 			   "mail.smtp.socketFactory.fallback":"false",
 			    "mail.debug": "true"]
