0180af217049a13099bf384ece59db655d83757f
weidewind <weidewind@gmail.com>
2014-11-19 17:36:35 +0300

    separate waiting page

--- grails-app/controllers/bcvapp/JobController.groovy
+++ grails-app/controllers/bcvapp/JobController.groovy
@@ -204,13 +204,15 @@ class JobController {
 			pool.shutdown()
 		})
 		def start = new Date(System.currentTimeMillis())
-		def murl = createLink(controller: 'job', action: 'waiting')
+		def murl = createLink(controller: 'job', action: 'waiting', params:[start:start, randomString: randomString])
 		render(contentType: 'text/html', text: "<script>window.location.href='$murl'</script>")
 		//render view: 'waiting'
 		//render "<p>Please, don't close this page. Your task was submitted at ${start}.</p>"
 		while (!pool.isTerminated()){
 			def randomString = jobService.talkWork()
-			render view: 'waiting', model:[start:start, randomString: randomString]
+			murl = createLink(controller: 'job', action: 'waiting', params:[start:start, randomString: randomString])
+			render(contentType: 'text/html', text: "<script>window.location.href='$murl'</script>")
+			//render view: 'waiting', model:[start:start, randomString: randomString]
 			//render "<p>${randomString}</p>"
 			sleep(5000)
 		}
