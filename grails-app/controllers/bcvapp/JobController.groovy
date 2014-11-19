2fdba69ad01e173e5ca06c17da8c104b34a5cc84
weidewind <weidewind@gmail.com>
2014-11-19 14:46:09 +0300

    separate waiting page

--- grails-app/controllers/bcvapp/JobController.groovy
+++ grails-app/controllers/bcvapp/JobController.groovy
@@ -165,11 +165,11 @@ class JobController {
 		else {
 			//render "Your task has been added to the queue"
 			def start = new Date(System.currentTimeMillis())
-			render (view: "waiting", model:[start: start, randomString: ""])
+			render (view: "waiting", model:[start:start, randomString:""])
 			def queueSize = Bcvjob.countByDateCreatedLessThanEquals(job.dateCreated) + Stapjob.countByDateCreatedLessThanEquals(job.dateCreated)
 			while (queueSize > 2){  // 1 running task + our task
 				def randomString = jobService.talkQueue()
-				 render (view: "waiting", model:[start: start, randomString: randomString])
+				 render (view: "waiting", model:[start:start, randomString:randomString])
 				//render "<p>${randomString}</p>"
 				sleep(5000)
 				queueSize = Bcvjob.countByDateCreatedLessThanEquals(job.dateCreated) + Stapjob.countByDateCreatedLessThanEquals(job.dateCreated)
@@ -203,11 +203,11 @@ class JobController {
 			pool.shutdown()
 		})
 		def start = new Date(System.currentTimeMillis())
-		render  (view: "waiting", model:[start: start, randomString: ""])
+		render  (view: "waiting", model:[start:start, randomString:""])
 		//render "<p>Please, don't close this page. Your task was submitted at ${start}.</p>"
 		while (!pool.isTerminated()){
 			def randomString = jobService.talkWork()
-			render (view: "waiting", model:[start: start, randomString: randomString]) 
+			render (view: "waiting", model:[start:start, randomString:randomString]) 
 			//render "<p>${randomString}</p>"
 			sleep(5000)
 		}
