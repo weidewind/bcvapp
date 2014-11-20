0788841d81c33e3e8bba87993fd1cb64fb885ad9
weidewind <weidewind@gmail.com>
2014-11-20 16:49:14 +0300

    separate waiting page

--- grails-app/services/bcvapp/HolderService.groovy
+++ grails-app/services/bcvapp/HolderService.groovy
@@ -8,10 +8,12 @@ class HolderService {
 	def jobDone = [:]
 	
     def setDone(String sessionId) {
+		println ( sessionId + " done ")
 		jobDone.putAt(sessionId, true)
     }
 	
 	def isDone(String sessionId) {
+		println ("Checking if " + sessionId + " done .. " + jobDone.get(sessionId))
 		return jobDone.get(sessionId)
 	}
 }
