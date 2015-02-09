6a86f9ff21ea133638117ff4b25011a6b739392b
weidewind <weidewind@gmail.com>
2015-02-09 14:09:03 +0300

    index redirect

--- grails-app/controllers/bcvapp/IndexController.groovy
+++ grails-app/controllers/bcvapp/IndexController.groovy
@@ -3,7 +3,7 @@ package bcvapp
 class IndexController {
 
     def index() {
-	//	redirect(controller: "bcvjob", action: "form") 
+		render redirect(controller: "bcvjob", action: "form") 
 		}
 	
 //	def bcvRedirect (){
